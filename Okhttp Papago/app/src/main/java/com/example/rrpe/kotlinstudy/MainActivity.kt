3560b8e864a6375f896f89be20668d28212d8603
Rrpe <kel_777@naver.com>
2018-11-12 16:48:34 +0900

    코틀린 Okhttp

---
+++ Okhttp Papago/app/src/main/java/com/example/rrpe/kotlinstudy/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.rrpe.kotlinstudy
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.gson.Gson
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val JSON = MediaType.parse("application/json; charset=utf-8")
+
+        val client = OkHttpClient()
+        var url = "https://openapi.naver.com/v1/papago/n2mt"
+        var json = JSONObject()
+        json.put("source", "ko")
+        json.put("target", "en")
+        json.put("text", "만나서 반갑습니다.")
+        val body = RequestBody.create(JSON, json.toString())
+        val request = Request.Builder()
+                .header("X-Naver-Client-Id", "u2opBtU70ZrvltCUoWpy")
+                .addHeader("X-Naver-Client-Secret", "KghUIcf0XB")
+                .url(url)
+                .post(body)
+                .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                var str = response!!.body()!!.string()
+                println(str)
+                var papagoDTO = Gson().fromJson<PapagoDTO>(str, PapagoDTO::class.java)
+                println(papagoDTO.message!!.result!!.translatedText)
+            }
+
+        })
+    }
+}
